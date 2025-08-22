8533951475312fa99807bebfd45ad32b5b2d0638
Vadim Vetrov <vetrovvd@gmail.com>
2025-08-22 19:11:44 +0300

    Add documentation

--- equations.cpp
+++ equations.cpp
@@ -8,7 +8,7 @@ static inline int is_zero(double n) {
 	return fabs(n) < DOUBLE_EPS;
 }
 
-int solve_linear(const double coeffs[2], double *root) {
+int solve_linear(const double coeffs[LINEAR_COEFFICIENTS_LEN], double *root) {
 	assert (coeffs	!= NULL);
 	assert (root	!= NULL);
 
@@ -28,17 +28,17 @@ int solve_linear(const double coeffs[2], double *root) {
 	return 1;
 }
 
-int solve_quadratic(const double coeffs[3], double roots[2]) {
+int solve_quadratic(const double coeffs[QUADRATIC_COEFFICIENTS_LEN], 
+		    double roots[MAX_QUADRATIC_ROOTS]) {
 	assert (coeffs	!= NULL);
 	assert (roots	!= NULL);
 
-	double x1 = 0, x2 = 0;
 	double	a = coeffs[0],
 		b = coeffs[1],
 		c = coeffs[2];
 
 	double discriminant = 0;
-	int roots_ct = 0;
+	int nRoots = 0;
 
 	if (is_zero(a)) {
 		return solve_linear(coeffs + 1, roots);
@@ -49,13 +49,15 @@ int solve_quadratic(const double coeffs[3], double roots[2]) {
 		return 0;
 	}
 
+	double x1 = 0, x2 = 0;
+
 	if (is_zero(discriminant)) {
-		roots_ct = 1;
+		nRoots = 1;
 
 		x1 = -b / (2 * a);
 		x2 = x1;
 	} else {
-		roots_ct = 2;
+		nRoots = 2;
 
        		discriminant = sqrt(discriminant);
 		x1 = (-b - discriminant) / (2 * a);
@@ -64,5 +66,5 @@ int solve_quadratic(const double coeffs[3], double roots[2]) {
 	
 	roots[0] = x1;
 	roots[1] = x2;
-	return roots_ct;
+	return nRoots;
 }
--- solver.cpp
+++ solver.cpp
@@ -1,58 +1,65 @@
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <assert.h>
 #include "equations.h"
 
 /**
- * Reads n double coefficients from stdin.
- * Ensures the coefficients are separated by spaces or new lines
+ * @brief Parses coefficients from stdin
+ *
+ * Reads \p len coefficients from stdin.
+ * Ensures the coefficients are separated by spaces or newlines.
+ *
+ * @param coeffs An array of \p len elements written with parsed coefficients
+ *
+ * @param len The length of \p coeffs array and the number of coefficients to parse
+ *
+ * @return 0 if parsed successfully, -1 in case of an error
  */
 int read_coefficients(double coeffs[], int len);
 
+/**
+ * @brief Writes found roots to stdout
+ *
+ * @param roots An array of \p nRoots length with roots of the equation
+ *
+ * @param nRoots The number of found roots. 
+ * If roots number is infinity, pass SQ_INF_ROOTS
+ *
+ * @return 0 if parsed successfully, -1 in case of an error
+ */
+int print_roots(const double roots[], int nRoots);
+
 int main() {
-	double coeffs[3] = {0};
-	double roots[2] = {0};
-	int nRoots = 0;
+	double coeffs[QUADRATIC_COEFFICIENTS_LEN] = {0};
 	int ret = 0;
 
 	printf(	"Write coefficients a, b, c "
 	       	"separated with spaces so they form "
 		"a quadratic equation a*x^2 + b*x + c = 0 :\n");
 
-
-	ret = read_coefficients(coeffs, 3);
+	ret = read_coefficients(coeffs, QUADRATIC_COEFFICIENTS_LEN);
 
 	if (ret) {
 		fprintf(stderr, "RTFM\n");
 		return EXIT_FAILURE;
 	}
 
-	nRoots = solve_quadratic(coeffs, roots);
-	if (nRoots == SQ_INF_ROOTS) {
-		printf("The equation is valid for every rational x\n");
-		return EXIT_SUCCESS;
-	}
+	double roots[MAX_QUADRATIC_ROOTS] = {0};
+	int nRoots = solve_quadratic(coeffs, roots);	
 
-	printf("Found a total of %d solutions of the given equation", nRoots);
-
-	if (nRoots != 0) {
-		printf(" : ");
-
-		for (int i = 0; i < nRoots; i++) {
-			printf("%lg ", roots[i]);
-		}
-	}
-	printf("\n");
+	print_roots(roots, nRoots);
 	
 	return EXIT_SUCCESS;
 }
 
 int read_coefficients(double coeffs[], int len) {
-	int ret = 0;
+	assert (coeffs != NULL);
+
 	int c = 0;
 
 	for (int i = 0; i < len; i++) {
-		ret = scanf("%lg", coeffs + i);
+		int ret = scanf("%lg", coeffs + i);
 		if (errno) {
 			perror("scanf() failed");
 			return -1;
@@ -71,6 +78,28 @@ int read_coefficients(double coeffs[], int len) {
 	if (c != '\n') {
 		return -1;
 	}
+
+	return 0;
+}
+
+int print_roots(const double roots[], int nRoots) {
+	assert (roots != NULL);
+
+	if (nRoots == SQ_INF_ROOTS) {
+		printf("The equation is valid for every rational x\n");
+		return 0;
+	}
+
+	printf("Found a total of %d solutions of the given equation", nRoots);
+
+	if (nRoots != 0) {
+		printf(" : ");
+
+		for (int i = 0; i < nRoots; i++) {
+			printf("%lg ", roots[i]);
+		}
+	}
+	printf("\n");
 
 	return 0;
 }
