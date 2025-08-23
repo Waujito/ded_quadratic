12ea9dd4999cdad2569f0c1cf6b97f4d4eb2477c
Vadim Vetrov <vetrovvd@gmail.com>
2025-08-23 11:54:06 +0300

    Remove code duplication

--- test/equations.cpp
+++ test/equations.cpp
@@ -1,138 +1,80 @@
 #include "equations.h"
+#include <math.h>
 #include <gtest/gtest.h>
 
+void assertion_check(double a, double b, double c, int nRoots, double x1, double x2);
 
-TEST(EqTest, TwoRealRoots)
-{
-	double coeffs[QUADRATIC_COEFFICIENTS_LEN] = {1, -5, 6};
+void assertion_check(double a, double b, double c, int nRoots, double x1, double x2) {
+	double coeffs[QUADRATIC_COEFFICIENTS_LEN] = {a, b, c};
 	double roots[MAX_QUADRATIC_ROOTS] = {0};
 
-	int nRoots = solve_quadratic(coeffs, roots);	
+	int nRoots_t = solve_quadratic(coeffs, roots);	
 
-	ASSERT_EQ(nRoots, 2);
-	ASSERT_DOUBLE_EQ(roots[0], 2);
-	ASSERT_DOUBLE_EQ(roots[1], 3);
+	ASSERT_EQ(nRoots, nRoots_t);
+	if (nRoots >= 1) {
+		ASSERT_DOUBLE_EQ(roots[0], x1);
+	}
+	if (nRoots >= 2) {
+		ASSERT_DOUBLE_EQ(roots[1], x2);
+	}
+}
+
+
+TEST(EqTest, TwoRealRoots) {
+	assertion_check(1, -5, 6, 2, 2, 3);
 }
 
 TEST(EqTest, NegativeCoefficients)
 {
-	double coeffs[QUADRATIC_COEFFICIENTS_LEN] = {2, 3, -2};
-	double roots[MAX_QUADRATIC_ROOTS] = {0};
-
-	int nRoots = solve_quadratic(coeffs, roots);	
-
-	ASSERT_EQ(nRoots, 2);
-	ASSERT_DOUBLE_EQ(roots[0], -2);
-	ASSERT_DOUBLE_EQ(roots[1], 0.5);
+	assertion_check(2, 3, -2, 2, -2, 0.5);
 }
 
 TEST(EqTest, DoubleRoot)
 {
-	double coeffs[QUADRATIC_COEFFICIENTS_LEN] = {1, -6, 9};
-	double roots[MAX_QUADRATIC_ROOTS] = {0};
-
-	int nRoots = solve_quadratic(coeffs, roots);	
-
-	ASSERT_EQ(nRoots, 1);
-	ASSERT_DOUBLE_EQ(roots[0], 3);
+	assertion_check(1, -6, 9, 1, 3, NAN);
 }
 
 TEST(EqTest, NoRationalSolutions)
 {
-	double coeffs[QUADRATIC_COEFFICIENTS_LEN] = {1, 4, 5};
-	double roots[MAX_QUADRATIC_ROOTS] = {0};
-
-	int nRoots = solve_quadratic(coeffs, roots);	
-
-	ASSERT_EQ(nRoots, 0);
+	assertion_check(1, 4, 5, 0, NAN, NAN);
 }
 
 TEST(EqTest, Zero_b)
 {
-	double coeffs[QUADRATIC_COEFFICIENTS_LEN] = {1, 0, -9};
-	double roots[MAX_QUADRATIC_ROOTS] = {0};
-
-	int nRoots = solve_quadratic(coeffs, roots);	
-
-	ASSERT_EQ(nRoots, 2);
-	ASSERT_DOUBLE_EQ(roots[0], -3);
-	ASSERT_DOUBLE_EQ(roots[1], 3);
+	assertion_check(1, 0, -9, 2, -3, 3);
 }
 
 TEST(EqTest, Zero_c)
 {
-	double coeffs[QUADRATIC_COEFFICIENTS_LEN] = {1, 5, 0};
-	double roots[MAX_QUADRATIC_ROOTS] = {0};
-
-	int nRoots = solve_quadratic(coeffs, roots);	
-
-	ASSERT_EQ(nRoots, 2);
-	ASSERT_DOUBLE_EQ(roots[0], -5);
-	ASSERT_DOUBLE_EQ(roots[1], 0);
+	assertion_check(1, 5, 0, 2, -5, 0);
 }
 
 TEST(EqTest, Zero_bc)
 {
-	double coeffs[QUADRATIC_COEFFICIENTS_LEN] = {4, 0, 0};
-	double roots[MAX_QUADRATIC_ROOTS] = {0};
-
-	int nRoots = solve_quadratic(coeffs, roots);	
-
-	ASSERT_EQ(nRoots, 1);
-	ASSERT_DOUBLE_EQ(roots[0], 0);
+	assertion_check(4, 0, 0, 1, 0, NAN);
 }
 
 TEST(EqTest, Large_coeffs)
 {
-	double coeffs[QUADRATIC_COEFFICIENTS_LEN] = {1e6, -4e6, 3e6};
-	double roots[MAX_QUADRATIC_ROOTS] = {0};
-
-	int nRoots = solve_quadratic(coeffs, roots);	
-
-	ASSERT_EQ(nRoots, 2);
-	ASSERT_DOUBLE_EQ(roots[0], 1);
-	ASSERT_DOUBLE_EQ(roots[1], 3);
+	assertion_check(1e6, -4e6, 3e6, 2, 1, 3);
 }
 
 TEST(EqTest, Negative_a)
 {
-	double coeffs[QUADRATIC_COEFFICIENTS_LEN] = {-1, 1, 2};
-	double roots[MAX_QUADRATIC_ROOTS] = {0};
-
-	int nRoots = solve_quadratic(coeffs, roots);	
-
-	ASSERT_EQ(nRoots, 2);
-	ASSERT_DOUBLE_EQ(roots[0], -1);
-	ASSERT_DOUBLE_EQ(roots[1], 2);
+	assertion_check(-1, 1, 2, 2, -1, 2);
 }
 
 TEST(EqTest, Linear_eq)
 {
-	double coeffs[QUADRATIC_COEFFICIENTS_LEN] = {0, 2, -4};
-	double roots[MAX_QUADRATIC_ROOTS] = {0};
-
-	int nRoots = solve_quadratic(coeffs, roots);	
-
-	ASSERT_EQ(nRoots, 1);
-	ASSERT_DOUBLE_EQ(roots[0], 2);
+	assertion_check(0, 2, -4, 1, 2, NAN);
 }
 
 TEST(EqTest, Linear_no_solution)
 {
-	double coeffs[QUADRATIC_COEFFICIENTS_LEN] = {0, 0, 7};
-	double roots[MAX_QUADRATIC_ROOTS] = {0};
-
-	int nRoots = solve_quadratic(coeffs, roots);	
-
-	ASSERT_EQ(nRoots, 0);
+	assertion_check(0, 0, 7, 0, NAN, NAN);
 }
 
 TEST(EqTest, Linear_infinity_solutions)
 {
-	double coeffs[QUADRATIC_COEFFICIENTS_LEN] = {0, 0, 0};
-	double roots[MAX_QUADRATIC_ROOTS] = {0};
-
-	int nRoots = solve_quadratic(coeffs, roots);	
-
-	ASSERT_EQ(nRoots, SQ_INF_ROOTS);
+	assertion_check(0, 0, 0, SQ_INF_ROOTS, NAN, NAN);
 }
